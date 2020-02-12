ceaacaa5fe7c9283ee88ccece2a0290de0083d43
akashmeshram <6388495akash@gmail.com>
2020-02-12 20:44:50 +0530

    Favicon added

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,15 @@ module.exports = {
         path: `${__dirname}/src/`,
       },
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Ogura Hyakunin Isshu`,
+        short_name: `Karuta`,
+        start_url: `https://akashmeshram.github.io/Ogura-Hyakunin-Isshu/`,
+        icon: `static/favicon.png`,
+      },
+    },
     `gatsby-transformer-json`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
